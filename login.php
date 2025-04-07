759b488371edc2583f90ca632fd0a2c588b558ba
Ippman <heroparth@gmail.com>
2025-04-07 19:36:48 +0530

    First commit.

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+if (isset($_SESSION['role']) && isset($_SESSION['id'])){
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Task Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-DQvkBjpPgn7RC31MCQoOeC9TI2kdqa4+BSgNMNj8v77fdC77Kj5zpWFTJaaAoMbC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+<body class="login-body">
+<form method="POST" action="app/login.php" class="shadow p-4">
+  <h3 class="display-4">LOGIN</h3>
+  <?php if (isset($_GET['error'])) {?>
+    <div class="alert alert-danger" role="alert">
+  <?php echo stripcslashes($_GET['error']); ?>
+</div>
+<?php } ?>
+<?php if (isset($_GET['success'])) {?>
+    <div class="alert alert-success" role="alert">
+  <?php echo stripcslashes($_GET['success']); ?>
+</div>
+<?php } 
+// $pass = 'password';
+// $pass = password_hash($pass, PASSWORD_DEFAULT);
+// echo $pass;
+?>
+
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">User name</label>
+    <input type="text" class="form-control" name="user_name">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-YUe2LzesAfftltw+PEaao2tjU/QATaW/rOitAq67e0CT0Zi2VVRL0oC4+gAaeBKu" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php } else{
+  $em = "Login First.";
+  header("Location: login.php?error=$em");
+  exit();
+}
+?>
